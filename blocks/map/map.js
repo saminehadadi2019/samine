4146b5081ce1fbb0ecad0ccd40c41bf3501e5094
Samine Hadadi <shadadi@samines-mbp.home>
2023-05-24 06:56:15 +1000

    map was updated

--- blocks/map/map.js
+++ blocks/map/map.js
@@ -5,26 +5,27 @@ export default function decorate(block) {
   const map = document.createElement('map');
   map.name = 'myMap'
   const image = document.createElement('img');
-  image.src = 'https://i.ibb.co/zrHVB4x/Screenshot-2023-05-22-at-11-49-29-PM.png';
+  image.src = 'https://i.ibb.co/S393wpr/map.png';
   image.alt = 'Image Description';
   image.useMap = '#myMap';
   
   const area1 = document.createElement('area');
-  area1.shape = 'rect';
-  area1.coords = '0,0,100,200';
+  area1.shape = 'circle';
+  area1.coords = '200,200,90';
   area1.addEventListener('click', () => {
-    console.log('Clicked on Area 1');
+    alert('Clicked on Area 1');
   });
   area1.style.borderBlockColor = 'red';
-  area1.href="#"
+area1.href="#"
   
   const area2 = document.createElement('area');
   area2.shape = 'circle';
-  area2.coords = '0,0,200';
+  area2.coords = '780,780,140';
   area2.addEventListener('click', () => {
-    console.log('Clicked on Area 2');
+    alert('Clicked on Area 2');
   });
   area2.style.backgroundColor = 'blue';
+area2.href="#"
   
   // image.addEventListener('click', () => {
   //   console.log("I'm he")
@@ -36,11 +37,12 @@ export default function decorate(block) {
   block.appendChild(image);
   block.appendChild(map);
   
+}
   
   //block.append(image);
   //block.append(map);
   //block.append(map);
-}
+
 
 
 // https://i.ibb.co/zrHVB4x/Screenshot-2023-05-22-at-11-49-29-PM.png
