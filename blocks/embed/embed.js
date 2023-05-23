502607a25b4b903b876e8045607947334b32fd6b
Samine Hadadi <shadadi@Samines-MacBook-Pro.local>
2023-05-23 12:38:11 +1000

    map added

--- blocks/embed/embed.js
+++ blocks/embed/embed.js
@@ -2,8 +2,8 @@ import { createOptimizedPicture } from '../../scripts/lib-franklin.js';
 
 export default function decorate(block) {
   const iframe = document.createElement('iframe');
-  iframe.width = '560';
-  iframe.height = '315';
+  iframe.width = '100%';
+  iframe.height = '600';
   iframe.src = `https://www.youtube.com/embed/JkWWjpULpjU`;
   block.append(iframe);
   //block.append(iframe);
